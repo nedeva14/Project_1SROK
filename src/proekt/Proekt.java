0a31e3f6ed70c6d952766ddb7bb8ec46206dc634
annasn <annasn@LAB27-6.school.npmg.org>
2022-12-20 15:15:50 +0200

    hello4

--- src/proekt/Proekt.java
+++ src/proekt/Proekt.java
@@ -14,9 +14,9 @@ public class Proekt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
-        int a=0;
-        int b=4;
+       
+        
+        
     }
     
 }
